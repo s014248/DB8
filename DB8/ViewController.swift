7af855c3a9d151d98637f0cc7714ba82d4c8863a
bob <s014248@students.lmsd.org>
2020-02-19 07:43:07 -0500

    Initial Commit

---
+++ DB8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DB8
+//
+//  Created by Alexander Zhang (student LM) on 2/19/20.
+//  Copyright © 2020 Alexander Zhang (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
